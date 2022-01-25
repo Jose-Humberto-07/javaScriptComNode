d3a6f593c278c53a9ae4c29a25d56252dec70063
José Humberto <jhpnascimento96@gmail.com>
2022-01-24 21:20:00 -0300

    component calculator

---
+++ front/react/calculadora/my-calculator/src/main/Calculator.jsx
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+import "./Calculator.css";
+
+/**
+ * 
+
+ */
+
+export default class Calculator extends Component {
+  render() {
+    return (
+      <div className="calculator">
+        <h1>messi</h1>
+      </div>
+    );
+  }
+}
+
+/**
+ * function Calculator () {
+  
+
+  return (
+    <div className="calculator">
+      <h1>hello</h1>
+    </div>
+  )
+}
+
+export default Calculator;
+ */
